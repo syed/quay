25dd18d496343d14911dd68886db6dc128eae3b8
Syed <syed@apache.org>
2024-12-14 21:58:27 -0500

    add hf endpoints

---
+++ artifacts/plugins/modelregistry/hf_utils.py
@@ -0,0 +1,6 @@
+def get_revision_sha(namespace, repo, revision):
+    # Pull the manifest from the database,
+    # and return the sha of the model which is
+    # stored as an annotation
+
+    pass
--- artifacts/plugins/modelregistry/modelregistry_routes.py
+++ artifacts/plugins/modelregistry/modelregistry_routes.py
@@ -1,12 +1,12 @@
 import logging
 
-from flask import Blueprint, request, jsonify, make_response, abort
-
-from artifacts.plugins.modelregistry import PLUGIN_NAME, URL_PREFIX
-from artifacts.plugins.modelregistry.modelregistry_models import ModelRegistryMetadata
-from auth.decorators import process_oauth
+from flask import Blueprint, abort, jsonify, make_response, request
 from peewee import fn
 
+from artifacts.plugins.modelregistry import PLUGIN_NAME, URL_PREFIX, hf_utils
+from artifacts.plugins.modelregistry.modelregistry_models import ModelRegistryMetadata
+
+from auth.decorators import process_oauth
 from data.database import db
 
 bp = Blueprint(PLUGIN_NAME, __name__)
@@ -25,7 +25,7 @@ def search(namespace, repo):
     query format: https://www.postgresql.org/docs/current/functions-json.html#FUNCTIONS-SQLJSON-FILTER-EX-TABLE
     """
 
-    query = request.json.get("query")
+    query = request.get_json().get("query")
     if not query:
         abort(400, "json path query is required")
 
@@ -36,6 +36,26 @@ def search(namespace, repo):
     res = [r.manifest.digest for r in rows]
     return jsonify(res), 200
 
+
+# START: Huggingface compatible API for fetching models (EXPERIMENTAL)
+@bp.route("/api/models/<namespace>/<repo>")
+def hf_model_info(namespace, repo):
+    revision = "main"  # default to main
+    return hf_model_info_by_revision(namespace, repo, revision)
+
+
+@bp.route("/api/models/<namespace>/<repo>/revision/<revision>")
+def hf_model_info_by_revision(namespace, repo, revision):
+    tag = revision
+    model_info = {
+        "id": f"{namespace}/{repo}",
+        "modelId": f"{namespace}/{repo}",
+        "sha": hf_utils.get_revision_sha(namespace, repo, tag),
+    }
+    return jsonify(model_info), 200
+
+
+# END: Huggingface compatible API for fetching models (EXPERIMENTAL)
 
 # STEP2
 # Cross repo query
